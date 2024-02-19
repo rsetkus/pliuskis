b4d062b8fe9865da3df70d630141df04fa2681ae
Robertas Setkus <robertas@setkus.lt>
2024-02-19 21:34:33 +0200

    Changes: - Added Jetpack Compose dependencies - Implemented Timber logging

--- app/src/main/kotlin/lt/setkus/pliuskis/MyApplication.kt
+++ app/src/main/kotlin/lt/setkus/pliuskis/MyApplication.kt
@@ -1,5 +1,13 @@
 package lt.setkus.pliuskis
 
 import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
-class MyApplication : Application()
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(DebugTree())
+    }
+}
