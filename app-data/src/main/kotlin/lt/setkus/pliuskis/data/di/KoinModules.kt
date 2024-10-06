4a50d85308723880e3991deed94fe54bf0aedc49
Robertas Setkus <robertas@setkus.lt>
2024-10-06 20:58:00 +0300

    Changes: - Added Kotlin Coroutines RxJava extension - Refactored Mqtt implementation to use HiveMQ library instead of AWS - Refactored KeyStore implementation to use PKCS12 instead of PEM

--- app-data/src/main/kotlin/lt/setkus/pliuskis/data/di/KoinModules.kt
+++ app-data/src/main/kotlin/lt/setkus/pliuskis/data/di/KoinModules.kt
@@ -4,9 +4,9 @@ import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager
 import lt.setkus.pliuskis.core.systemstate.SystemStateRequestable
 import lt.setkus.pliuskis.data.BuildConfig
 import lt.setkus.pliuskis.data.IotManager
-import lt.setkus.pliuskis.data.aws.AwsIotManager
-import lt.setkus.pliuskis.data.crypto.AwsKeyStoreProducer
-import lt.setkus.pliuskis.data.crypto.KeyStoreProducer
+import lt.setkus.pliuskis.data.mqttclient.HiveMqttClientManager
+import lt.setkus.pliuskis.data.mqttclient.getDebugMqttClient
+import lt.setkus.pliuskis.data.mqttclient.getProductionMqttClient
 import lt.setkus.pliuskis.data.systemstate.AwsSystemStateRequest
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -16,12 +16,9 @@ val dataModule = module {
         AWSIotMqttManager("pliuskis", BuildConfig.AMAZON_IOT_ENDPOINT)
     }
 
-    single<KeyStoreProducer> {
-        AwsKeyStoreProducer(androidContext())
-    }
-
     single<IotManager> {
-        AwsIotManager(get(), get())
+        val client = if (BuildConfig.DEBUG) getDebugMqttClient() else getProductionMqttClient(androidContext())
+        HiveMqttClientManager(client)
     }
 
     factory<SystemStateRequestable> { AwsSystemStateRequest(get()) }
