b10c378bb122f8bd7f100f0d6de38f8a6b471213
Robertas Setkus <robertas@setkus.lt>
2025-03-04 21:34:04 +0800

    Implemented LoadingWheel and loading screen.

--- feature/devices/src/main/kotlin/lt/setkus/pliuskis/feature/devices/DeviceListScreen.kt
+++ feature/devices/src/main/kotlin/lt/setkus/pliuskis/feature/devices/DeviceListScreen.kt
@@ -1,12 +1,14 @@
 package lt.setkus.pliuskis.feature.devices
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
@@ -18,10 +20,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import lt.setkus.pliuskis.core.designsystem.component.LoadingWheel
 import org.koin.androidx.compose.koinViewModel
 import timber.log.Timber
 
@@ -47,12 +53,11 @@ internal fun DeviceListScreen(
     devices: DevicesListScreenState,
     modifier: Modifier = Modifier
 ) {
-    val gridState = rememberLazyGridState()
     val itemsList = remember { mutableListOf<DeviceListItem>() }
 
     when (devices) {
         is DevicesListScreenState.Error -> Timber.e("Error: ${devices.error}")
-        DevicesListScreenState.Loading -> Unit
+        DevicesListScreenState.Loading -> LoadingDevicesScreen(modifier)
         is DevicesListScreenState.Success -> {
             itemsList.add(devices.device)
             Box(
@@ -64,8 +69,7 @@ internal fun DeviceListScreen(
                 ) {
                     devicesFeed(
                         devices = itemsList,
-                        onDeviceClick,
-                        modifier = modifier
+                        onDeviceClick
                     )
                 }
             }
@@ -73,10 +77,32 @@ internal fun DeviceListScreen(
     }
 }
 
+@Composable
+@Suppress("MagicNumber")
+private fun LoadingDevicesScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            LoadingWheel(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .wrapContentSize()
+                    .testTag("forYou:loading"),
+                contentDesc = stringResource(id = R.string.devices_loading),
+            )
+        }
+    }
+}
+
 fun LazyStaggeredGridScope.devicesFeed(
     devices: List<DeviceListItem>,
     onDeviceClick: (String) -> Unit,
-    modifier: Modifier = Modifier
 ) {
     items(
         items = devices,
